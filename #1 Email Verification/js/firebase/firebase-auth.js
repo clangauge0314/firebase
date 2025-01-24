025807be1bba8c31ba384d01fc675809c4f35b26
clangauge0314 <clangauge0314@naver.com>
2025-01-24 14:57:23 +0900

    3-2. firebase-auth.js 생성

---
+++ #1 Email Verification/js/firebase/firebase-auth.js
@@ -0,0 +1,57 @@
+import { auth } from "./firebase-init.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
+// 회원가입 함수
+export const signup = async (email, password, displayName) => {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+  await updateProfile(userCredential.user, {
+    displayName: displayName,
+  });
+
+  await sendEmailVerification(userCredential.user);
+
+  return userCredential.user;
+};
+
+// 로그인 함수
+export const login = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    if (!userCredential.user.emailVerified) {
+      throw new Error("이메일 인증이 필요합니다. 이메일을 확인해주세요.");
+    }
+
+    console.log("로그인 성공: ", userCredential.user);
+    return userCredential.user;
+  } catch (error) {
+    if (error.code === "auth/invalid-credential") {
+      throw new Error("이메일 또는 비밀번호가 올바르지 않습니다.");
+    } else if (error.code === "auth/too-many-requests") {
+      throw new Error(
+        "너무 많은 로그인 시도가 있었습니다. 잠시 후 다시 시도해주세요."
+      );
+    } else {
+      throw error;
+    }
+  }
+};
+
+export const resetPassword = async (email) => {
+    await sendPasswordResetEmail(auth, email);
+}
