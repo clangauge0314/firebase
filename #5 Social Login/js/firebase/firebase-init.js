90a4419e1b9b4e249d3ee2ce6565517bf280c6fd
clangauge0314 <clangauge0314@naver.com>
2025-01-26 13:38:54 +0900

    7-3. firebase 깃허브 로그인 설정 및 함수 추가

--- #5 Social Login/js/firebase/firebase-init.js
+++ #5 Social Login/js/firebase/firebase-init.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 const firebaseConfig = {
-  // 여러분들의 firebaseConfig로 덮어쓰기 해주세요.
+  // 여러분들의 firebaseConfig로 덮어쓰기 해주세요..
 };
 
 const app = initializeApp(firebaseConfig);
